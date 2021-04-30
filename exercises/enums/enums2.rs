06d76361d546ee2ffb3d659f5114905a1b1aa5ab
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2021-04-30 14:28:10 +0300

    Some more solutions

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,12 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move{x: u32, y: u32},
+    Echo(String),
+    ChangeColor(u32, u32, u32),
+    Quit
 }
 
 impl Message {
@@ -25,4 +26,12 @@ fn main() {
     for message in &messages {
         message.call();
     }
+
+    let lel = disemvowel("This website is for losers LOL!");
+    print!("{}", lel);
+}
+
+fn disemvowel(s: &str) -> String {
+    let vowels = vec!['a','e','i','o','u','A','E','I','O','U'];
+    s.chars().filter(|c| !vowels.contains(c)).collect()
 }
